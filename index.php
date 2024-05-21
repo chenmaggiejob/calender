52ce98232e40fcbc813c984820da23d33d19ced3
maggiechen <chenmaggiejob@gmail.com>
2024-05-21 16:27:47 +0800

    切版與當月日曆程式碼

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,18 +8,128 @@
   <link rel="shortcut icon" href="#">
   <title>萬年曆作業</title>
   <style>
-   /*請在這裹撰寫你的CSS*/ 
-    
+    * {
+      box-sizing: border-box;
+      border-collapse: collapse;
+    }
+
+    .container {
+      width: 80%;
+      /* height: 300px; */
+      margin: auto;
+      background-color: lightcoral;
+      display: flex;
+    }
+
+    .contentL {
+      width: 30%;
+      /* height: 100%; */
+      background-color: black;
+    }
+
+    .contentR {
+      width: 70%;
+      /* height: 100%; */
+      background-color: lightgray;
+      display: flex;
+      flex-wrap: wrap;
+    }
+
+    .title,
+    .item {
+      width: 14%;
+      height: 45px;
+      margin: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid black;
+
+    }
+
+    .holiday {
+      background-color: lightpink;
+    }
   </style>
 </head>
-<body>
-<h1>萬年曆</h1>  
 
-<?php
-/*請在這裹撰寫你的萬年曆程式碼*/  
-  
-  
-?>
-  
+<body>
+  <?php
+  // 如果_GET有傳值回來，變數month就會等於傳回來的值，否則month就會等於當月
+  $month = $_GET['month'] ?? date('m');
+
+  // 如果_GET有傳值回來，變數year就會等於傳回來的值，否則year就會等於當年
+  $year = $_GET['year'] ?? date('Y');
+
+  //取得當月1號的日期
+  $firstday = date("$year-$month-01");
+
+  //在將當月1號的日期格式轉成時間戳
+  $firstday_stemp = strtotime($firstday);
+
+  //找當月份的1號是星期幾(如果date裡面要計算時間，必需要使用時間戳)
+  $firstWeekStartDay = date("w", $firstday_stemp);
+
+  //找當月份總共有幾天
+  $days = date("t", $firstday_stemp);
+
+  //找到當月最後一天
+  $lastday = date("$year-$month-$days");
+
+  //將當月的最後一天轉成時間戳
+  $lastday_stemp = strtotime($lastday);
+
+  //先設一個可以儲存每個日期的陣列
+  $days = [];
+
+  //月份的最大週數為6週，6*7=42所以i值設定<42
+  for ($i = 0; $i < 42; $i++) {
+    $diff = $i - $firstWeekStartDay;
+    $days[] = date("Y-m-d", strtotime("$diff day", $firstday_stemp));
+  }
+
+  // echo "<pre>";
+  // print_r($data);
+  // echo "</pre>";
+
+  ?>
+
+
+  <div class="container">
+    <div class="contentL">
+
+    </div>
+    <div class="contentR">
+      <?php
+      $str = "日 一 二 三 四 五 六";
+      $title = explode(" ", $str);
+      foreach ($title as $a) {
+        echo "<div class='title'> $a </div>";
+      }
+
+      foreach ($days as $day) {
+        $date = explode("-", $day)[2];
+        $whatDay = date("w", strtotime($day)); //判斷是否為六日
+        if ($whatDay == 0 || $whatDay == 6) {
+          echo "<div class='item holiday'> $date </div>";
+        } else {
+          echo "<div class ='item'> $date </div>";
+        }
+      }
+
+
+
+
+      ?>
+    </div>
+  </div>
+
+  <?php
+
+
+
+  ?>
+
 </body>
+
 </html>
